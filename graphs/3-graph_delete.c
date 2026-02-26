c04cabb6dde2deba204d0a10e687873f2de1417b
Aimable Nkurikiyimana <aimablenkurikiyimana1@gmail.com>
2026-02-26 16:28:43 +0200

    004

--- graphs/3-graph_delete.c
+++ graphs/3-graph_delete.c
@@ -3,6 +3,7 @@
 void graph_delete(graph_t *graph)
 {
     vertex_t *vertex;
+    edge_t *edges;
 
     if (!graph)
         return;
@@ -11,6 +12,12 @@ void graph_delete(graph_t *graph)
     while (vertex)
     {
         vertex_t *next = vertex->next;
+        while(edges) {
+            edge_t *node = edges;
+            edges = node->next;
+            free(node);
+            edges = edges->next;
+        }
         free(vertex->content);
         free(vertex);
         vertex = next;
