343c9261ee64ea2f33acf9f61c7f4a9c1535888c
Aimable Nkurikiyimana <aimablenkurikiyimana1@gmail.com>
2026-02-26 15:20:58 +0200

    refining delete function

--- graphs/3-graph_delete.c
+++ graphs/3-graph_delete.c
@@ -1,11 +1,14 @@
 #include "graphs.h"
 
 void graph_delete(graph_t *graph) {
+
+    edge_t *edges;
+    vertex_t *vertex;
+
     if (!graph)
         return;
-    vertex_t *vertex = graph->vertices;
-    edge_t *edges;
 
+    vertex = graph->vertices;
     while(vertex) {
         edges = vertex->edges;
         while(edges) {
