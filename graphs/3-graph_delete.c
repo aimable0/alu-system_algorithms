7d6ba5861a1d8f97641459d6bbf1a2de4811884e
Aimable Nkurikiyimana <aimablenkurikiyimana1@gmail.com>
2026-02-26 16:24:40 +0200

    003

--- graphs/3-graph_delete.c
+++ graphs/3-graph_delete.c
@@ -1,21 +1,19 @@
 #include "graphs.h"
 
-void graph_delete(graph_t *graph) {
-
+void graph_delete(graph_t *graph)
+{
     vertex_t *vertex;
 
     if (!graph)
         return;
 
     vertex = graph->vertices;
-    while(vertex) {
+    while (vertex)
+    {
         vertex_t *next = vertex->next;
-        printf(" ...Deleting vertex: %s\n", vertex->content);
         free(vertex->content);
         free(vertex);
         vertex = next;
-        graph->nb_vertices -= 1;
-        printf(" ...vertex remaining: %zu\n", graph->nb_vertices);
     }
     free(graph);
 }
